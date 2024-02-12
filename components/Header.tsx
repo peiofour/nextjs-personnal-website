65bc6aca1f17a3e4abbd3f71584a7b623310d7d3
Pierre Fournier <peiofour@gmail.com>
2024-02-12 17:05:19 +0100

    wip

---
+++ components/Header.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+
+export function Header() {
+  return (
+    <header className='p-4 flex items-center mb-10'>
+      <div className='container'>
+        <div className='text-4xl font-extrabold no-underline flex-1 z-20'>
+          pierre.
+        </div>
+        <nav></nav>
+      </div>
+    </header>
+  )
+}
