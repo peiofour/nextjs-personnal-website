e952da1f9614b9eafda8f11e084db71e7aaa4101
pierrefournier <peiofour@gmail.com>
2024-04-09 15:24:22 +0200

    feat: add dependencies for react-icons and sharp, and create robots.ts and sitemap.ts files

---
+++ app/sitemap.ts
@@ -0,0 +1,24 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+	return [
+		{
+			url: 'https://www.pierrefournier.dev/',
+			lastModified: new Date(),
+			changeFrequency: 'yearly',
+			priority: 1.0,
+		},
+    {
+      url: 'https://www.pierrefournier.dev/projects',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://www.pierrefournier.dev/contact',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.7,
+    }
+	]
+}
