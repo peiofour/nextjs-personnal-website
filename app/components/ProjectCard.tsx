0c27ca6b767b8533cf4cad1b5e427265fd3fa9d9
pierrefournier <peiofour@gmail.com>
2024-05-15 16:30:14 +0200

    refactor: update ProjectCard component to use NextUI Card component
    Refactor the ProjectCard component to use the Card component from the NextUI library. This improves the overall design and consistency of the project cards. Additionally, update the imports and remove unused dependencies.

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -1,5 +1,12 @@
-import Image from 'next/image'
-import Link from 'next/link'
+import NextImage from 'next/image'
+import {
+	Card,
+	Image,
+	CardHeader,
+	CardBody,
+	CardFooter,
+	Chip,
+} from '@nextui-org/react'
 
 export interface IProjectCard {
 	title: string
@@ -23,71 +30,31 @@ export function ProjectCard({
 	demo,
 }: IProjectCard) {
 	return (
-		<div
-			className='flex h-[780px] max-w-[400px] flex-col gap-4 rounded-lg bg-white
-      shadow-card transition-all duration-200 ease-in-out
-      hover:shadow-card-hover'
-		>
-			<div className='relative h-[300px] w-full overflow-hidden rounded-t-lg bg-gray-300'>
+		<Card className='max-w-[400px] p-3'>
+			<CardHeader>
 				<Image
-					fill
-					style={{
-						objectFit: 'cover',
-					}}
+					as={NextImage}
 					src={images[0]}
 					alt={alt}
+					width={400}
+					height={300}
+					className='h-[250px] object-cover'
+					shadow='md'
 				/>
-			</div>
-			<div className='flex flex-[2] flex-col pb-4 pl-4 pr-8 text-center'>
-				<div className='flex-1'>
-					<h3 className='pb-1 text-xl font-bold'>{title}</h3>
-					<p className='pb-3 text-base font-light leading-normal'>
-						{description}
-					</p>
+			</CardHeader>
+			<CardBody>
+				<h3 className='mb-4 text-xl font-semibold'>{title}</h3>
+				<p className='text-gray-500'>{description}</p>
+			</CardBody>
+			<CardFooter className='flex flex-col gap-3'>
+				<div className='flex flex-wrap gap-3'>
+					{tags.map((tag) => (
+						<Chip size='sm' color='default' key={tag}>
+							{tag}
+						</Chip>
+					))}
 				</div>
-				<div className='flex flex-col gap-3'>
-					<div className='flex flex-wrap items-center justify-center gap-3'>
-						{tags.map((tag) => (
-							<span
-								key={tag}
-								className='rounded-lg bg-gray-200 px-2 py-1 text-sm'
-								tabIndex={0}
-							>
-								{tag}
-							</span>
-						))}
-					</div>
-					<div className='flex items-center justify-center gap-2 pt-3'>
-						{website && (
-							<Link
-								className='rounded-lg px-2 py-1 text-blue-500 transition-colors duration-300 ease-in-out hover:bg-blue-100 hover:text-blue-700'
-								href={website}
-								target='_blank'
-							>
-								Site web
-							</Link>
-						)}
-						{source && (
-							<Link
-								className='rounded-lg px-2  py-1 text-blue-500 transition-colors duration-300 ease-in-out hover:bg-blue-100 hover:text-blue-700'
-								href={source}
-								target='_blank'
-							>
-								Source
-							</Link>
-						)}
-						{demo && (
-							<Link
-								className='rounded-lg px-2  py-1 text-blue-500 transition-colors duration-300 ease-in-out hover:bg-blue-100 hover:text-blue-700'
-								href={demo}
-								target='_blank'
-							>
-								Démo
-							</Link>
-						)}
-					</div>
-				</div>
-			</div>
-		</div>
+			</CardFooter>
+		</Card>
 	)
 }
